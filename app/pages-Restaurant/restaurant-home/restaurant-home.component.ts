fbca85140412a5cde6f370865af7717feffd5f1c
Boyrie <florian.boyrie@imerir.com>
2017-12-01 16:27:18 +0000

    Propre 1

--- app/pages-Restaurant/restaurant-home/restaurant-home.component.ts
+++ app/pages-Restaurant/restaurant-home/restaurant-home.component.ts
@@ -1,24 +1,24 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {Resto} from '../../restaurant-service/restaurant-service';
+import {Resto,RestoService} from '../../restaurant-service/restaurant-service';
 
 @Component({
     moduleId:     module.id,
     selector:    'osl-restaurant-home',
-    templateUrl: 'restaurant-home.component.html'
+    templateUrl: 'restaurant-home.component.html',
+    providers: [RestoService]
 })
 export default class RestaurantHomeComponent implements OnInit, OnDestroy {
 
     resto: Resto;
-    displayPrice: boolean;
-
     subscriberParams: any;
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(private restoService: RestoService, private route: ActivatedRoute) {}
 
     ngOnInit() {
         this.subscriberParams = this.route.params.subscribe(params => {
             let id: number = +params['id'];   // (+) converts string 'id' to a number
+            this.resto = this.restoService.getRestosById(id);
         });
 
     }
