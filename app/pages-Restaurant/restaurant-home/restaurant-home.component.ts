43fa2581ac01b9cfacab243f723757af8359a8c6
Boyrie <florian.boyrie@imerir.com>
2017-12-02 02:03:55 +0000

    Application V1.0 ok

--- app/pages-Restaurant/restaurant-home/restaurant-home.component.ts
+++ app/pages-Restaurant/restaurant-home/restaurant-home.component.ts
@@ -3,54 +3,45 @@ import {ActivatedRoute} from '@angular/router';
 import {Resto,RestoService} from '../../restaurant-service/restaurant-service';
 
 @Component({
-    moduleId:     module.id,
+    moduleId: module.id,
     selector:    'osl-restaurant-home',
     templateUrl: 'restaurant-home.component.html',
-<<<<<<< HEAD
-=======
     styleUrls:  ['restaurant-home.component.css'],
->>>>>>> page2
     providers: [RestoService]
 })
 export default class RestaurantHomeComponent implements OnInit, OnDestroy {
 
     resto: Resto;
-<<<<<<< HEAD
-=======
-    restoServ: RestoService; //Suppr Router
->>>>>>> page2
-    subscriberParams: any;
+    displayPrice: boolean;
     imageUrl: string;
     tagsString: string = "";
 
-<<<<<<< HEAD
+    subscriberParams: any;
+    subscriberData: any;
+
     constructor(private restoService: RestoService, private route: ActivatedRoute) {}
-=======
-    //constructor(private restoService: RestoService, private route: ActivatedRoute) {}
-    constructor() {}
->>>>>>> page2
 
     ngOnInit() {
-        /*this.subscriberParams = this.route.params.subscribe(params => {
+        this.subscriberParams = this.route.params.subscribe(params => {
             let id: number = +params['id'];   // (+) converts string 'id' to a number
             this.resto = this.restoService.getRestosById(id);
-<<<<<<< HEAD
+            this.imageUrl = '/images/' + id + '.jpg';
         });
-=======
-        });*/
-        this.restoServ = new RestoService()            //Suppr Router
-        this.resto = this.restoServ.getRestosById(0);  //Suppr Router
 
-        this.imageUrl = '/images/' + 0 + '.jpg';
+        this.subscriberData = this.route.data.subscribe(data => {
+            this.displayPrice = data['displayPrice'];
+        });
+
+        //Display Array Tags
         for (let t of this.resto.tag)
         {
           this.tagsString += t + " * ";
         }
->>>>>>> page2
 
     }
 
     ngOnDestroy() {
         this.subscriberParams.unsubscribe();
+        this.subscriberData.unsubscribe();
     }
 }
